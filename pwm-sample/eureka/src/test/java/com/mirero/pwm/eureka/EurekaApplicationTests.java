2f78424cf6e0cde18097229e97e725123c574c3b
김진수 <jinsu.kim@mirero.co.kr>
2025-01-21 17:50:37 +0900

    feat: eureka service 개발

---
+++ pwm-sample/eureka/src/test/java/com/mirero/pwm/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirero.pwm.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
