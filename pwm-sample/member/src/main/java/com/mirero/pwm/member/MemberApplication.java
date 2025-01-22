d5712a606d183b64b0dcd952433878f3bc185320
김진수 <jinsu.kim@mirero.co.kr>
2025-01-22 10:20:30 +0900

    feat: MSA 환경에서 하나의 swagger 문서로 통합

--- pwm-sample/member/src/main/java/com/mirero/pwm/member/MemberApplication.java
+++ pwm-sample/member/src/main/java/com/mirero/pwm/member/MemberApplication.java
@@ -1,12 +1,14 @@
 package com.mirero.pwm.member;
 
 import com.mirero.pwm.common.exception.CommonExceptionConfig;
+import com.mirero.pwm.common.swagger.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(CommonExceptionConfig.class)
+//@Import({CommonExceptionConfig.class, SwaggerConfig.class})
+@Import({CommonExceptionConfig.class})
 public class MemberApplication {
 
     public static void main(String[] args) {
