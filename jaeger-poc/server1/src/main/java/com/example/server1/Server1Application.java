844185a84a1433d2a3475f70ce2fdb37b829144f
김진수 <jinsu.kim@mirero.co.kr>
2025-02-19 16:53:38 +0900

    feat: port 4318로 지정, opentelemetry-javaagent 사용해서 jaeger 띄우기 성공

--- jaeger-poc/server1/src/main/java/com/example/server1/Server1Application.java
+++ jaeger-poc/server1/src/main/java/com/example/server1/Server1Application.java
@@ -1,5 +1,10 @@
 package com.example.server1;
 
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import io.opentelemetry.sdk.trace.export.SpanExporter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -17,4 +22,22 @@ public class Server1Application {
         return new RestTemplate();
     }
 
+//    @Bean
+//    public OpenTelemetry openTelemetry() {
+//        // Span Exporter 설정 (OTLP 사용)
+//        SpanExporter spanExporter = OtlpGrpcSpanExporter.builder()
+//            .setEndpoint("http://localhost:4319") // OTLP gRPC 수집기
+//            .setCompression("gzip")
+//            .build();
+//
+//        // Tracer Provider 설정
+//        SdkTracerProvider tracerProvider = SdkTracerProvider.builder()
+//            .addSpanProcessor(BatchSpanProcessor.builder(spanExporter).build())
+//            .build();
+//
+//        // OpenTelemetry SDK 설정
+//        return OpenTelemetrySdk.builder()
+//            .setTracerProvider(tracerProvider)
+//            .buildAndRegisterGlobal();
+//    }
 }
