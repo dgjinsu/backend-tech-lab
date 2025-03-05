f045b90dc96aff2774047a593e4f4cb39204756e
dgjinsu <rlawlstn3206@naver.com>
2025-03-05 16:39:35 +0900

    first commit

---
+++ jacoco-multi-module/src/test/java/com/example/jacocomultimodule/JacocoMultiModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jacocomultimodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacocoMultiModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
