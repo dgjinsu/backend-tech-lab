63f45c4e9972bd6252293b0ea258272396864149
김진수 <jinsu.kim@mirero.co.kr>
2025-03-18 13:06:14 +0900

    feat: multi module 환경 구축

---
+++ grafana-tempo/src/test/java/com/example/grafanatempo/GrafanaTempoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grafanatempo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrafanaTempoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
