92724c03542bc0f87a999acc51ea31dd35e0ae0c
김진수 <jinsu.kim@mirero.co.kr>
2025-03-19 16:59:03 +0900

    feat: tempo로 보내는 부분 해결 필요. zipkin, jaeger, prometheus 등은 정상 작동

---
+++ grafana-tempo/server1/src/main/java/com/example/server1/Member.java
@@ -0,0 +1,24 @@
+package com.example.server1;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+}
--- grafana-tempo/server1/src/main/java/com/example/server1/ServerOneController.java
+++ grafana-tempo/server1/src/main/java/com/example/server1/ServerOneController.java
@@ -1,17 +1,27 @@
 package com.example.server1;
 
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 public class ServerOneController {
 
-    @GetMapping("/server1")
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/save")
     public String server1() {
       log.info("Server 1");
+      memberRepository.save(Member.builder().name("name").build());
       return "Server 1";
     }
 
+    @GetMapping("/get")
+    public List<Member> server2() {
+        return memberRepository.findAll();
+    }
 }
