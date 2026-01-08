98a090a4bf00de97db27b56a5553aadbdb63d196
김진수 <jinsu.kim@mirero.co.kr>
2026-01-08 10:44:41 +0900

    feat: first commit

---
+++ nexus-sse/src/test/java/com/example/nexussse/NexusSseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nexussse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusSseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
