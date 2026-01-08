f503fd34f3d62522151b8e4031756e19df2c5985
김진수 <jinsu.kim@mirero.co.kr>
2026-01-08 13:57:17 +0900

    feat: first commit

---
+++ nexus-notification-service/src/main/java/com/example/nexusnotificationservice/NexusNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.nexusnotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexusNotificationServiceApplication.class, args);
+    }
+
+}
---
+++ nexus-notification-service/src/test/java/com/example/nexusnotificationservice/NexusNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nexusnotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusNotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
