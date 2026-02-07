cd0d4312787a7ab0450923770e96545a2412b219
김진수 <rlawlstn3206@gmail.com>
2026-02-07 16:36:34 +0900

    feat: first commit

---
+++ architecture-study/src/test/java/com/example/architecturestudy/ArchitectureStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.architecturestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
